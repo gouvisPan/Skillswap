fb73671c896b661ba610d8e3ea8e0c7e0e3a34cb
Panagiotis Gouvis <gouvispan@gmai.com>
2022-11-29 20:52:06 +0200

    profile

--- client/src/pages/Profile/subPages/Skills/Skills.tsx
+++ client/src/pages/Profile/subPages/Skills/Skills.tsx
@@ -6,16 +6,25 @@ import "./Skills.scss";
 
 const Skills = () => {
   const [displaySingle,setDisplaySingle] = useState(false);
-  
+  const [chosenSkill , setChosenSkill] = useState()
   const user = useAppSelector(state => state.user.data);
-  
+
+  const singleSkillJSX = <div className="single-skill">
+
+  </div>
+
+  const allSKillsJSX = <div className="skills-container">
+                          {user?.mySkills.map((skill) => (
+                            <SkillCard skill={skill} />
+                          ))}
+                          <SkillCard skill={{ name: "X", sp: 0, desc: "",experience: {info : "Native Speaker", years: 25} }} />
+                        </div>
+
+
+  const content = displaySingle ? singleSkillJSX : allSKillsJSX
+
   return (
-    <div className="skills-container">
-      {user?.mySkills.map((skill) => (
-        <SkillCard skill={skill} />
-      ))}
-      <SkillCard skill={{ name: "X", sp: 0, desc: "",experience: {info : "Native Speaker", years: 25} }} />
-    </div>
+    content
   );
 };
 
